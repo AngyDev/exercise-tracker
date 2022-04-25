62f9279c7198096570e7d61c26d9b09b27b843d0
Angela Busato <busato.angela@gmail.com>
2022-04-25 17:22:43 +0200

    feat: add users router file

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
 
+app.use(express.json());
+
+app.use("/api/users", require("./routes/users"));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
-})
+});
 
 app.listen(port, host, () => {
   console.log(`Server listening on ${host}:${port}`);
-})
+});
